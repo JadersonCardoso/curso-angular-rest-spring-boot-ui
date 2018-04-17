a20f38f742945501fc3fc3815a810d90458322cc
Jaderson <jaderson.brandao@gmail.com>
2018-04-17 19:20:02 -0300

    12.6. Two-way binding com ngModel

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -8,11 +14,15 @@ import { NgForm } from '@angular/forms';
 })
 export class FormularioComponent {
 
+  cliente = new Cliente();
   profissoes = ['Programador', 'Engenheiro', 'Outra'];
 
   salvar(form: NgForm) {
-    console.log(form);
-    console.log(form.value.nome);
+    //this.cliente.nome = form.value.primeiroNome;
+    //this.cliente.email = form.value.emailAddress;
+    //this.cliente.profissao = form.value.profissao;
+    console.log(form.value);
+    console.log(this.cliente);
   }
 
 }
